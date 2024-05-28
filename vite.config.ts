2860158ec35fdc1d06b4611f2c038e0875e17e00
Xiaoping Hu <31302150+XiaopingHu@users.noreply.github.com>
2024-05-28 12:35:14 +0800

    【添加】添加注释

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import { VantResolver, VantImports } from '@vant/auto-import-resolver';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(),AutoImport({
+    // 注释此选项，可正常导入函数式组件的样式
     imports:[VantImports()],
     resolvers:[VantResolver()]
   }),
